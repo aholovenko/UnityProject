de0e75788378fbc71157d59313a54616934d2d42
aholovenko <aholovenko@hotmail.com>
2017-09-11 16:07:24 +0300

    Commit 1
    Start of a project, background, walking, turning around

---
+++ Assets/Scripts/HeroRabbit.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeroRabbit : MonoBehaviour {
+
+    public float speed = 1;
+
+    Rigidbody2D body = null;
+
+	// Use this for initialization
+	void Start () {
+        body = this.GetComponent<Rigidbody2D>();
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        float val = Input.GetAxis("Horizontal");
+
+        if (Mathf.Abs(val) > 0)
+        {
+            Vector2 vel = body.velocity;
+            vel.x = speed * val;
+            body.velocity = vel;
+        }
+
+        SpriteRenderer sr = GetComponent<SpriteRenderer>();
+        if (val > 0)
+            sr.flipX = false;
+        else if (val < 0)
+            sr.flipX = true;
+    }
+}
